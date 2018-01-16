79709ae12f89bb4baffd867b915d806492398bb4
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-01-16 11:34:14 -0500

    update routes

--- routes/routes.js
+++ routes/routes.js
@@ -12,15 +12,28 @@ const users = require('../controllers/users')
 */
 router.route('/users')
   .post(users.createUser)
+
+router.route('/users/:userId/id')
   .get(auth.validateUser, users.getUserById)
   .put(auth.validateUser, users.updateUser)
   .delete(auth.validateUser, users.deleteUser)
 
+router.route('/users/:email/email')
+  .get(users.getUserByEmail)
+
+router.route('/cities')
+  .post(cities.createCity)
+
+router.route('/cities/:cityId/id')
+  .get(cities.getCityById)
+  .put(cities.updateCity)
+
+
 /*
 * Auth Routes
 */
-router.route('/auth/login')
-  .post(auth.loginUser);
+// router.route('/auth/login')
+//   .post(auth.loginUser);
 
 // expose routes through router object
 module.exports = router;
